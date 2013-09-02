e3edab5569faa19008476c2fd99a6d11a1ce80fc
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-09-02 17:58:40 +0100

    Customise bootstrap cols

--- gangplank/grails-app/conf/ApplicationResources.groovy
+++ gangplank/grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,19 @@
 modules = {
-    application {
-        resource url:'js/application.js'
+  application {
+    resource url:'js/application.js'
+    // resource url:'js/bootstrap-editable.min.js'
+  }
+  editable {
+    // resource url:'css/select2.css'
+    // resource url:'js/editable.js.gsp'
+    // resource url:'js/moment.min.js'
+    // resource url:'js/select2.min.js'
+  }
+  overrides {
+    'bootstrap-css' {
+      resource id: 'bootstrap-css', url:'/css/gangplank-bootstrap-2.min.css'
+      resource id: 'bootstrap-responsive-css', url:'/css/gangplank-bootstrap-2-responsive.min.css'
     }
+  }
 }
+
