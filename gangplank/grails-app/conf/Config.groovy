afd982e4b7109e339fb639b9b1c2089d676b9cc6
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-09-07 12:59:43 +0100

    Added some code to process submissions, added logging config

--- gangplank/grails-app/conf/Config.groovy
+++ gangplank/grails-app/conf/Config.groovy
@@ -69,25 +69,42 @@ environments {
     }
 }
 
-// log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console appender:
+    // Example of changing the log pattern for the default console
+    // appender:
     //
     //appenders {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+    appenders {
+        console name: "stdout", threshold: org.apache.log4j.Level.ALL
+    }
+
+//    // Enable Hibernate SQL logging with param values
+//    trace 'org.hibernate.type'
+//    debug 'org.hibernate.SQL'
+
+    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+           'org.codehaus.groovy.grails.web.pages', //  GSP
+           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+           'org.codehaus.groovy.grails.web.mapping', // URL mapping
+           'org.codehaus.groovy.grails.commons', // core / classloading
+           'org.codehaus.groovy.grails.plugins', // plugins
+           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+
+    debug  'grails.app.controllers',
+           'grails.app.service',
+           'grails.app.services',
+           'grails.app.domain',
+           'grails.app.tagLib',
+           'grails.app.conf',
+           'grails.app.jobs',
+           'grails.app.conf.BootStrap'
 }
 
 // Added by the Spring Security Core plugin:
--- gangplank/grails-app/controllers/gangplank/SubmitController.groovy
+++ gangplank/grails-app/controllers/gangplank/SubmitController.groovy
@@ -3,4 +3,14 @@ package gangplank
 class SubmitController {
 
     def index() { }
+
+  def processSubmission() {
+    if ( request.method == 'POST' ) {
+      def upload_mime_type = request.getFile("submissionFile")?.contentType
+      def upload_filename = request.getFile("submissionFile")?.getOriginalFilename()
+      def input_stream = request.getFile("submissionFile")?.inputStream
+      log.debug("Done");
+    }
+  }
+
 }
