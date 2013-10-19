ac34639149f8c2b098fe6233721e9e736a143670
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-10-19 16:08:50 +0100

    updated to grails 2.3.1

--- gangplank/grails-app/conf/BuildConfig.groovy
+++ gangplank/grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
 
 
     plugins {
-        runtime ':hibernate:3.6.10.M3' 
+        runtime ':hibernate:3.6.10.2' 
         runtime ":jquery:1.8.3"
         runtime ":resources:1.2"
 
@@ -59,7 +59,7 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
 
-        build ':tomcat:7.0.40.1'
+        build ':tomcat:7.0.42'
 
         runtime ":database-migration:1.3.2"
 
